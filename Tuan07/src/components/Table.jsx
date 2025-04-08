4058f18c242aeaf90f1bd68c9f4435b68e354af7
loc2605 <locphanhuu2605@gmail.com>
2025-04-09 00:03:35 +0700

    DataTable React JS

--- Tuan07/src/components/Table.jsx
+++ Tuan07/src/components/Table.jsx
@@ -1,9 +1,63 @@
 import React from 'react'
-
+const data = [
+  {
+    name: 'Elizabeth Lee',
+    company: 'AvatarSystems',
+    value: '$359',
+    date: '10/07/2023',
+    status: 'New',
+    avatar: 'Avatar.png'
+  },
+  {
+    name: 'Carlos Garcia',
+    company: 'SmoozeShift',
+    value: '$747',
+    date: '24/07/2023',
+    status: 'New',
+    avatar: 'Avatar (1).png'
+  },
+  {
+    name: 'Elizabeth Bailey',
+    company: 'Prime Time Telecom',
+    value: '$564',
+    date: '08/08/2023',
+    status: 'In-progress',
+    avatar: 'Avatar (2).png'
+  },
+  {
+    name: 'Ryan Brown',
+    company: 'OmniTech Corporation',
+    value: '$541',
+    date: '31/08/2023',
+    status: 'In-progress',
+    avatar: 'Avatar (3).png'
+  },
+  {
+    name: 'Ryan Young',
+    company: 'DataStream Inc.',
+    value: '$769',
+    date: '01/05/2023',
+    status: 'Completed',
+    avatar: 'Avatar (4).png'
+  },
+  {
+    name: 'Hailey Adams',
+    company: 'FlowRush',
+    value: '$922',
+    date: '10/06/2023',
+    status: 'Completed',
+    avatar: 'Avatar (5).png'
+  }
+];
+const statusColor = {
+  'New': 'text-blue-500 bg-blue-100',
+  'In-progress': 'text-yellow-600 bg-yellow-100',
+  'Completed': 'text-green-600 bg-green-100'
+};
 function Table() {
   return (
     <div>
-      <div className="flex items-center gap-2 mb-4">
+      <div className="flex items-center gap-2 mb-4 p-3">
         <img src="File text 1.png" alt="overview icon" className="w-5 h-5" />
         <h5 className="text-lg font-semibold">Detailed Report</h5>
         <div className="flex gap-2">
@@ -17,54 +71,38 @@ function Table() {
           </button>
         </div>
       </div>
-      
       <div>
-      <table className="w-300 border border-gray-600 text-center text-l">
+      <table className="min-w-full text-l p-2">
         <thead>
-          <tr>
-            <th className="border border-gray-600 px-2 py-">Column 1</th>
-            <th className="border border-gray-600 px-2 py-1">Column 2</th>
-            <th className="border border-gray-600 px-2 py-1">Column 3</th>
-            <th className="border border-gray-600 px-2 py-1">Column 4</th>
-            <th className="border border-gray-600 px-2 py-1">Column 5</th>
+          <tr className="text-left text-gray-600 bg-gray-100 border-b">
+            <th className="py-2">CUSTOMER NAME</th>
+            <th className="py-2">COMPANY</th>
+            <th className="py-2">ORDER VALUE</th>
+            <th className="py-2">ORDER DATE</th>
+            <th className="py-2">STATUS</th>
+            <th className="py-2"></th>
           </tr>
         </thead>
-        <tbody className="leading-tight">
-          <tr>
-            <td className="border border-gray-600 px-2 py-1">Row1-1</td>
-            <td className="border border-gray-600 px-2 py-1">Row1-2</td>
-            <td className="border border-gray-600 px-2 py-1">Row1-3</td>
-            <td className="border border-gray-600 px-2 py-1">Row1-4</td>
-            <td className="border border-gray-600 px-2 py-1">Row1-5</td>
-          </tr>
-          <tr>
-            <td className="border border-gray-600 px-2 py-1">Row2-1</td>
-            <td className="border border-gray-600 px-2 py-1">Row2-2</td>
-            <td className="border border-gray-600 px-2 py-1">Row2-3</td>
-            <td className="border border-gray-600 px-2 py-1">Row2-4</td>
-            <td className="border border-gray-600 px-2 py-1">Row2-5</td>
-          </tr>
-          <tr>
-            <td className="border border-gray-600 px-2 py-1">Row3-1</td>
-            <td className="border border-gray-600 px-2 py-1">Row3-2</td>
-            <td className="border border-gray-600 px-2 py-1">Row3-3</td>
-            <td className="border border-gray-600 px-2 py-1">Row3-4</td>
-            <td className="border border-gray-600 px-2 py-1">Row3-5</td>
-          </tr>
-          <tr>
-            <td className="border border-gray-600 px-2 py-1">Row4-1</td>
-            <td className="border border-gray-600 px-2 py-1">Row4-2</td>
-            <td className="border border-gray-600 px-2 py-1">Row4-3</td>
-            <td className="border border-gray-600 px-2 py-1">Row4-4</td>
-            <td className="border border-gray-600 px-2 py-1">Row4-5</td>
-          </tr>
-          <tr>
-            <td className="border border-gray-600 px-2 py-1">Row5-1</td>
-            <td className="border border-gray-600 px-2 py-1">Row5-2</td>
-            <td className="border border-gray-600 px-2 py-1">Row5-3</td>
-            <td className="border border-gray-600 px-2 py-1">Row5-4</td>
-            <td className="border border-gray-600 px-2 py-1">Row5-5</td>
-          </tr>
+        <tbody>
+          {data.map((row, index) => (
+            <tr key={index} className=" hover:bg-gray-50">
+              <td className="flex items-center gap-2 py-2">
+                <img src={row.avatar} alt="" className="w-8 h-8 rounded-full" />
+                <span className='font-bold'>{row.name}</span>
+              </td>
+              <td>{row.company}</td>
+              <td>{row.value}</td>
+              <td>{row.date}</td>
+              <td>
+                <span className={`px-3 py-2 rounded-full text-sm font-medium ${statusColor[row.status]}`}>
+                  {row.status}
+                </span>
+              </td>
+              <td>
+                <button>✏️</button>
+              </td>
+            </tr>
+          ))}
         </tbody>
       </table>
     </div>
